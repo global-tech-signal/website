e6a17e714377654d366229be499b2ae122710ab8
Abrar Ajaz Wani <abhaywani114@gmail.com>
2026-05-11 00:49:46 +0530

    Use netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel';
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://globaltechsignal.com",
+  site: 'https://globaltechsignal.com',
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 });
