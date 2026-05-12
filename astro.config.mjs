c072433638adadeae14ad322cefa68d8d3670eed
Abrar Ajaz Wani <manage@globaltechsignal.com>
2026-05-12 23:59:54 +0530

    Sitemap test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://globaltechsignal.com',
-  integrations: [tailwind()],
-  output: 'server',
+  //site: 'http://localhost:4321',
+  integrations: [tailwind(), sitemap()],
+  output: 'static',
   adapter: netlify(),
 });
